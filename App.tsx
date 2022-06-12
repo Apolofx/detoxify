81f2e53e0f43d1c3ffa86c533b612375552a9774
Nacho <nacho.conso@gmail.com>
2022-06-12 17:37:55 -0300

    register device for push notifications

--- App.tsx
+++ App.tsx
@@ -3,37 +3,26 @@
 //Navigators
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { registerForPushNotificationsAsync } from "@utils";
 //UI Provider
 import { NativeBaseProvider } from "native-base";
 import { NativeBaseConfig } from "@config";
 //Screens
 import { SCREENS } from "@config";
-import {
-  Home,
-  Auth,
-  Profile,
-  Settings,
-  Statistics,
-  Support,
-  Team,
-} from "@screens";
+import { Home, Auth, Settings, Support } from "@screens";
 
 //Helpers
 import { useAuthentication } from "@hooks";
 
 import { IconButton } from "@components";
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
+registerForPushNotificationsAsync()
 function App() {
   const authentication = useAuthentication();
-  const { isAuthenticated, isLoading } = authentication;
+  const { isAuthenticated } = authentication;
   return (
     <NativeBaseProvider config={NativeBaseConfig}>
       {!isAuthenticated ? (
@@ -41,7 +30,7 @@ function App() {
       ) : (
         <NavigationContainer>
           <Drawer.Navigator
-            initialRouteName="Home"
+            initialRouteName={SCREENS.HOME}
             screenOptions={{
               headerRight: () => (
                 <IconButton
@@ -53,9 +42,6 @@ function App() {
             }}
           >
             <Drawer.Screen name={SCREENS.HOME} component={Home} />
-            <Drawer.Screen name={SCREENS.PROFILE} component={Profile} />
-            <Drawer.Screen name={SCREENS.STATS} component={Statistics} />
-            <Drawer.Screen name={SCREENS.TEAM} component={Team} />
             <Drawer.Screen name={SCREENS.SETTINGS} component={Settings} />
             <Drawer.Screen name={SCREENS.SUPPORT} component={Support} />
           </Drawer.Navigator>
