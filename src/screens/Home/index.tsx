e7d5948c07a4bf6c1ef7777e33f7073b0c7d09d1
Nacho <nacho.conso@gmail.com>
2022-06-12 17:40:37 -0300

    add bottom tab to home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,46 +1,47 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
 import { DrawerScreenProps } from "@react-navigation/drawer";
-import { Timer } from "@components";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SCREENS } from "@config";
+import { Profile, Statistics, Team } from "../index";
+import { Ionicons } from "@expo/vector-icons";
 
 type HomeProps = DrawerScreenProps<RootParamList, "Home">;
 
-export default function Home({ navigation }: HomeProps) {
-  const fetchUser = async () => {
-    try {
-      const user = await fetch("http://192.168.1.21:8080/users/1").then((res) =>
-        res.json()
-      );
-      console.log(JSON.stringify(user));
-    } catch (e) {
-      console.log(JSON.stringify(e));
-    }
-  };
+const Tab = createBottomTabNavigator();
 
-  React.useEffect(() => {
-    fetchUser();
-  });
+export default function Home({ navigation, route }: HomeProps) {
   return (
-    <>
-      <SafeAreaView
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      >
-        <Text>Home Screen</Text>
-        <Timer />
-        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
-          <Text>Profile</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Team")}>
-          <Text>Team</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Statistics")}>
-          <Text>Statistics</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
-          <Text>Settings</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </>
+    <Tab.Navigator
+      screenOptions={{ tabBarActiveTintColor: "purple", headerShown: false, tabBarLabelPosition: "beside-icon"}}
+      initialRouteName="Perfil"
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, focused, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+        name="Perfil"
+        component={Profile}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, focused, size }) => (
+            <Ionicons name="stats-chart" color={color} size={size} />
+          ),
+        }}
+        name="Estadísticas"
+        component={Statistics}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, focused, size }) => (
+            <Ionicons name="people" color={color} size={size} />
+          ),
+        }}
+        name="Equipo"
+        component={Team}
+      />
+    </Tab.Navigator>
   );
 }
